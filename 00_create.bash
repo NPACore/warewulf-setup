9cfaa080570696c680d73cbd50d39f77effc2156
WillForan <willforan@gmail.com>
2025-05-29 09:07:39 -0400

    add checks to make scripts idempotent

--- 00_create.bash
+++ 00_create.bash
@@ -1,20 +1,34 @@
+#!/usr/bin/env bash
+#
+# create 'head' and 'node01' debian VMs to experiment with
+#
 # 20250514 - init
 
+# locally, for qemu's os-varient to know what 'debian12' is
 # sudo apt install osinfo-db-tools
 # sudo osinfo-db-import --latest -v
 
 
 # https://wiki.debian.org/KVM#Installation
-#
-virt-install --virt-type kvm \
+# SHOULD HAVE USED:
+# https://cloud.debian.org/images/cloud/bookworm-backports/20250428-2096/debian-12-backports-genericcloud-amd64-20250428-2096.qcow2
+virsh list --all | grep head ||
+ dryrun virt-install --virt-type kvm \
    --name "head" \
    --location https://deb.debian.org/debian/dists/bookworm/main/installer-amd64/ \
    --os-variant debian12 \
-   --disk size=5 \
+   --disk size=10 \
    --memory 2048 \
    --graphics none \
    --console pty,target_type=serial \
    --extra-args "console=ttyS0"
 
 # https://docs.redhat.com/en/documentation/red_hat_enterprise_linux/7/html/virtualization_deployment_and_administration_guide/sect-network_booting_with_libvirt-booting_a_guest_using_pxe#sect-Booting_a_guest_using_PXE-Using_bridged_networking
-# virt-install --pxe --network network=default --name "node01" --memory 1024 --graphics none --console pty,target_type=serial --disk size=2
+virsh list --all | grep node01 ||
+  dryrun virt-install \
+    --network network=default \
+    --pxe `# Network Boot` \
+    --name "node01" \
+    --disk size=5 \
+    --memory 1024 \
+    --graphics none --console pty,target_type=serial
--- 01_install.sh
+++ 01_install.sh
@@ -1,17 +1,33 @@
-apt install \
-  build-essential curl unzip \
-  git golang libnfs-utils libgpgme-dev libassuan-dev
+#!/usr/bin/env bash
+#
+# intended to be run on debian based 'head' node
+#
+# 1. get golang if needed
+# 2. install warewulf from source
+#
+
+command -v  go ||
+  apt install \
+    build-essential curl unzip \
+    git golang libnfs-utils libgpgme-dev libassuan-dev
 
 # update go
-go install golang.org/dl/go1.23.9@latest
-export PATH="$HOME/go/bin:$PATH"
-ln -fs $HOME/go/bin/go1.23.9 $HOME/go/bin/go
-go download
+if ! [[ $(go version) =~ 1.23.9 ]] ; then 
+   go install golang.org/dl/go1.23.9@latest
+   export PATH="$HOME/go/bin:$PATH"
+   ln -fs $HOME/go/bin/go1.23.9 $HOME/go/bin/go
+   go download
+fi
 
-test -d warewulf || git clone https://github.com/warewulf/warewulf.git
-cd warewulf
-git checkout main
-make all && make install
+# get warewulf
+if ! test -d warewulf; then
+   git clone https://github.com/warewulf/warewulf.git
+   cd warewulf
+   git checkout main
+   make all && make install
+fi
 
-# start service
+# config, only if out of date
+make /usr/local/etc/warewulf/warewulf.conf
+# start
 systemctl enable --now warewulfd
