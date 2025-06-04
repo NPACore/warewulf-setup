b2a8909c48374c28aee43b899fea31c0ff615c5a
Will Foran <willforan@gmail.com>
2025-06-04 14:43:03 -0400

    network boots!

--- 01_install.sh
+++ 01_install.sh
@@ -17,7 +17,7 @@ command -v  go ||
     nfs-kernel-server tftpd-hpa isc-dhcp-server \
     build-essential curl unzip \
     git golang libnfs-utils libgpgme-dev libassuan-dev \
-    slurm
+    slurmd slurmctld
 
 # dhcpcd in qemu failing
 # > No subnet declaration for enp1s0 (10.0.2.15).
@@ -33,9 +33,9 @@ if ! [[ $(go version) =~ 1.23.9 ]] ; then
 fi
 
 # get warewulf
-if ! test -d ~/warewulf; then
-   git clone https://github.com/warewulf/warewulf.git ~/warewulf
-   cd ~/warewulf
+if ! test -d /opt/warewulf; then
+   git clone https://github.com/warewulf/warewulf.git /opt/warewulf
+   cd /opt/warewulf
    git checkout main
    make all && dryrun make install
 fi
@@ -51,13 +51,25 @@ dryrun wwctl configure --all
 if ! wwctl profile list -j | jq '.default."image name"' | grep debian; then
   dryrun wwctl image import docker://ghcr.io/warewulf/warewulf-debian:12.0 debian-12.0
   dryrun wwctl profile set default --image=debian-12.0
+  # 20250602: this wasn't run initially?!
+  dryrun wwctl profile set --yes --image debian-12.0 "default"
 fi
 
-IF=enp1s0 #eth0
+IF=enp6s0f0 #enp1s0 #eth0
 GW=10.141.0.0 # CHECK ME. matches warewulf.conf
 dryrun wwctl profile set \
 	--yes --netdev $IF \
-	--netmask 255.255.255.0 \
+	--netmask 255.0.0.0 \
 	--gateway $GW "default"
 
-dryrun wwctl node add node01.cluster --ipaddr 10.141.0.1 --discoverable true
+dryrun wwctl node add node01 --ipaddr 10.141.0.1 --discoverable=true
+dryrun wwctl node add node0[2-4] --ipaddr=10.141.0.2 --discoverable=true
+#dryrun wwctl overlay build
+# creates debian-12.0.img
+#dryrun wwctl image exec debian-12.0 '/bin/ls'
+#https://kb.ciq.com/article/warewulf/ww-set-root-password
+dryrun wwctl image exec debian-12.0 -- /usr/bin/passwd root
+# edit allow root login editting service
+dryrun wwctl overlay edit wwclient etc/systemd/system/wwclient.service
+
+dryrun wwctl overlay build
--- Makefile
+++ Makefile
@@ -1,3 +1,8 @@
 /usr/local/etc/warewulf/warewulf.conf:  warewulf.conf
 	cp $< $@
 
+# reverse from above b/c always edit with
+#  wwctl node edit
+#  wwctl profile edit
+nodes.conf: /usr/local/etc/warewulf/nodes.conf
+	cp $< $@
